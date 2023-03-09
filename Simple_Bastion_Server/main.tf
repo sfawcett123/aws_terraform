651f7e124142a84be4dc214a071f068bff3f21da
Steven Fawcett <steve.fawcett@capgemini.com>
2023-03-09 11:40:13 +0000

    remove cloud watch

--- Simple_Bastion_Server/main.tf
+++ Simple_Bastion_Server/main.tf
@@ -1,11 +1,6 @@
 # Call our simple VPC module
 data "aws_region" "current" {}
 
-resource "aws_cloudwatch_log_group" "example" {
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = 1
-}
-
 module "vpc" {
     source = "../modules/vpc"
 }
@@ -13,10 +8,14 @@ module "vpc" {
 module "subnet" {
     source = "../modules/subnet"
     vpc_id = module.vpc.vpc_id
+  
+    subnet_cidrs = [ "10.0.1.0/24" ]
 }
 
 module "instance" {
     source = "../modules/instances"
     vpc_id = module.vpc.vpc_id
     subnet_id = module.subnet.subnet_id[0]
+
+    depends_on = [ module.subnet ]
 }
