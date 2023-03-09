c546b9d1e04eb6e6a2734c0ab2b865828433c154
Steven Fawcett <steve.fawcett@capgemini.com>
2023-03-09 12:04:23 +0000

    Allow AMI Name to be passed in

--- modules/instances/data.tf
+++ modules/instances/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = [ var.ami_name ]
   }
 
   filter {
--- modules/instances/input.tf
+++ modules/instances/input.tf
@@ -15,3 +15,10 @@ variable "default_tags" {
         environment_type = "Dev Environment"
     }
 }
+
+variable ami_name {
+    type = string
+    description = "AMI Name (Optional) Defaults to ubuntu"
+    default = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+}
+
